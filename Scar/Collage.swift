7a4879f3cce81ad69e52a67fc93014ef0219e79f
scarsTeam <antonio.cimino97@libero.it>
2020-04-29 15:48:17 +0200

    share

--- Scar/Collage.swift
+++ Scar/Collage.swift
@@ -58,86 +58,60 @@ class Collage: UIViewController, UITextFieldDelegate  {
     
     
     @IBAction func shareImg(_ sender: Any) {
-        
-        
-        let firstActivityItem = "\(String(describing: descriptionField.text))"
-            let secondActivityItem : NSURL = NSURL(string: "http//:hangme")!
-            // If you want to put an image
-        let image : UIImage =  scarsImage.image!
-
-            let activityViewController : UIActivityViewController = UIActivityViewController(
-                activityItems: [firstActivityItem, secondActivityItem, image], applicationActivities: nil)
-
-            // This lines is for the popover you need to show in iPad
-            activityViewController.popoverPresentationController?.sourceView = (sender as! UIButton)
-
-            // This line remove the arrow of the popover to show in iPad
-            activityViewController.popoverPresentationController?.permittedArrowDirections = UIPopoverArrowDirection.up
-            activityViewController.popoverPresentationController?.sourceRect = CGRect(x: 150, y: 150, width: 0, height: 0)
-
-            // Anything you want to exclude
-            activityViewController.excludedActivityTypes = [
-                UIActivity.ActivityType.postToWeibo,
-                UIActivity.ActivityType.print,
-                UIActivity.ActivityType.assignToContact,
-                UIActivity.ActivityType.saveToCameraRoll,
-                UIActivity.ActivityType.addToReadingList,
-                UIActivity.ActivityType.postToFlickr,
-                UIActivity.ActivityType.postToVimeo,
-                UIActivity.ActivityType.postToTencentWeibo,
-                UIActivity.ActivityType.postToFacebook,
-                UIActivity.ActivityType.postToTwitter,
-                
-            ]
-
-            self.present(activityViewController, animated: true, completion: nil)
+        guard let instagramUrl = URL(string: "instagram://app") else {
+            return
         }
-    @IBAction func shareUnkown(_ sender: Any) {
-         
-         
-         let ourRecord = CKRecord(recordType: "PublicInfo")
-                 
-                 ourRecord.setObject(descriptionField?.text as __CKRecordObjCValue?, forKey: "Description")
-                 
-                 self.activityField.startAnimating()
-                 
-         
-                 
-                 let mngr = FileManager.default
-                 let dir = mngr.urls(for: .documentDirectory, in: .userDomainMask)
-                 let file = dir[0].appendingPathComponent("myImage").path
-                 
-                 
-                  do {
-                     try scarsImage.image?.jpegData(compressionQuality: 0.5)?.write(to: URL(fileURLWithPath: file))
-                     let url = NSURL.fileURL(withPath: file)
-                     let imageAsset = CKAsset(fileURL: url)
-                     
-                     ourRecord.setObject(imageAsset as CKAsset, forKey: "Photo")
-                     print("\(url)")
-                  } catch _ {
-                     print("error")
-                 }
-                 
-                 
-                 dataBase.save(ourRecord,completionHandler:  {  (record, error) -> Void in
-                     
-                     if error != nil {
-
-                         print("error to save" + error.debugDescription)
-                         
-                     } else {
-
-                         print("save succesfull")
-                     }
-                     
-                     
-                     DispatchQueue.main.async{
-                         self.activityField.stopAnimating()
-                     }
-                 })
-        
+        if UIApplication.shared.canOpenURL(instagramUrl) {
+            guard let imageData = scarsImage.image!.jpegData(compressionQuality: 100) else {
+                return
+            }
+            let path = (NSTemporaryDirectory() as NSString).appendingPathComponent("instagram.ig")
+            let fileUrl = URL(fileURLWithPath: path)
+            do {
+                try imageData.write(to: fileUrl, options: .atomic)
+            } catch {
+                return
+            }
+            let documentController = UIDocumentInteractionController(url: fileUrl)
+            documentController.delegate = self as? UIDocumentInteractionControllerDelegate
+            documentController.uti = "com.instagram.photo"
+            documentController.presentOpenInMenu(from: self.view.frame, in: self.view, animated: true)
+        } else {
+            print("Istagram no istalled")
+        }
     }
-    
 
+    
+    @IBAction func shareUnkown(_ sender: Any) {
+        let ourRecord = CKRecord(recordType: "PublicInfo")
+        ourRecord.setObject(descriptionField?.text as __CKRecordObjCValue?, forKey: "Description")
+        self.activityField.startAnimating()
+                 
+        let mngr = FileManager.default
+        let dir = mngr.urls(for: .documentDirectory, in: .userDomainMask)
+        let file = dir[0].appendingPathComponent("myImage").path
+                 
+        do {
+            try scarsImage.image?.jpegData(compressionQuality: 0.5)?.write(to: URL(fileURLWithPath: file))
+            let url = NSURL.fileURL(withPath: file)
+            let imageAsset = CKAsset(fileURL: url)
+                     
+            ourRecord.setObject(imageAsset as CKAsset, forKey: "Photo")
+            print("\(url)")
+        } catch _ {
+            print("error")
+        }
+                 
+        dataBase.save(ourRecord,completionHandler:  {(record, error) -> Void in
+            if error != nil {
+                print("error to save" + error.debugDescription)
+            } else {
+                print("save succesfull")
+            }
+                     
+            DispatchQueue.main.async{
+                self.activityField.stopAnimating()
+            }
+        })
+    }
 }
