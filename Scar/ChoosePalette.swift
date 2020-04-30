19118a5ab170494e1b0f7e75024d7199bd2804c0
scarsTeam <antonio.cimino97@libero.it>
2020-04-30 17:47:04 +0200

    step3

--- Scar/ChoosePalette.swift
+++ Scar/ChoosePalette.swift
@@ -11,21 +11,25 @@ import CloudKit
 
 class ChoosePalette: UIViewController  {
    
-    override func viewDidLoad() {}
-
+    override func viewDidLoad() {
+        nex.isEnabled = false
+    }
+    
     @IBOutlet weak var cool: UIButton!
     @IBOutlet weak var warm: UIButton!
     @IBOutlet weak var neutral: UIButton!
-
+    @IBOutlet weak var nex: UIButton!
     static var color: String!
     
     @IBAction func chooseCool(_ sender: UIButton) {
         if sender.isSelected {
             cool.isSelected = false
+            nex.isEnabled = false
         } else {
             cool.isSelected = true
             warm.isSelected = false
             neutral.isSelected = false
+            nex.isEnabled = true
             ChoosePalette.color = "cool"
         }
     }
@@ -33,21 +37,25 @@ class ChoosePalette: UIViewController  {
     @IBAction func chooseWarm(_ sender: UIButton) {
         if sender.isSelected {
             warm.isSelected = false
+            nex.isEnabled = false
         } else {
             cool.isSelected = false
             warm.isSelected = true
             neutral.isSelected = false
+            nex.isEnabled = true
             ChoosePalette.color = "warm"
         }
     }
 
     @IBAction func chooseNeutral(_ sender: UIButton) {
         if sender.isSelected {
-             neutral.isSelected = false
+            neutral.isSelected = false
+            nex.isEnabled = false
         } else {
             cool.isSelected = false
             warm.isSelected = false
             neutral.isSelected = true
+            nex.isEnabled = true
             ChoosePalette.color = "neutral"
         }
     }
--- Scar/ChooseShape2.swift
+++ Scar/ChooseShape2.swift
@@ -11,53 +11,62 @@ import CloudKit
 
 class ChooseShape2: UIViewController  {
    
-    override func viewDidLoad() {}
+    override func viewDidLoad() {
+        nex.isEnabled = false
+    }
 
     @IBOutlet weak var square: UIButton!
     @IBOutlet weak var circle: UIButton!
     @IBOutlet weak var triangle: UIButton!
     @IBOutlet weak var rect: UIButton!
     @IBOutlet weak var zigZag: UIButton!
+    @IBOutlet weak var nex: UIButton!
     
     static var shape2: String!
     
     @IBAction func chooseSquare(_ sender: UIButton) {
-        if sender.isSelected {
+       if sender.isSelected {
             square.isSelected = false
+            nex.isEnabled = false
         } else {
             square.isSelected = true
             circle.isSelected = false
             triangle.isSelected = false
             rect.isSelected = false
             zigZag.isSelected = false
+            nex.isEnabled = true
             ChooseShape2.shape2 = "square"
             print(ChooseShape.shape1)
         }
     }
     
     @IBAction func chooseCircle(_ sender: UIButton) {
-        if sender.isSelected {
+       if sender.isSelected {
             circle.isSelected = false
+            nex.isEnabled = false
         } else {
             square.isSelected = false
             circle.isSelected = true
             triangle.isSelected = false
             rect.isSelected = false
             zigZag.isSelected = false
+            nex.isEnabled = true
             ChooseShape2.shape2 = "circle"
             print(ChooseShape.shape1)
         }
     }
     
     @IBAction func chooseTriangle(_ sender: UIButton) {
-        if sender.isSelected {
+      if sender.isSelected {
             triangle.isSelected = false
+            nex.isEnabled = false
         } else {
             square.isSelected = false
             circle.isSelected = false
             triangle.isSelected = true
             rect.isSelected = false
             zigZag.isSelected = false
+            nex.isEnabled = true
             ChooseShape2.shape2 = "triangle"
             print(ChooseShape.shape1)
         }
@@ -66,26 +75,30 @@ class ChooseShape2: UIViewController  {
     @IBAction func chooseRect(_ sender: UIButton) {
         if sender.isSelected {
             rect.isSelected = false
+            nex.isEnabled = false
         } else {
             square.isSelected = false
             circle.isSelected = false
             triangle.isSelected = false
             rect.isSelected = true
             zigZag.isSelected = false
+            nex.isEnabled = true
             ChooseShape2.shape2 = "rect"
             print(ChooseShape.shape1)
         }
     }
     
     @IBAction func chooseZigZag(_ sender: UIButton) {
-        if sender.isSelected {
+       if sender.isSelected {
             zigZag.isSelected = false
+            nex.isEnabled = false
         } else {
             square.isSelected = false
             circle.isSelected = false
             triangle.isSelected = false
             rect.isSelected = false
             zigZag.isSelected = true
+            nex.isEnabled = true
             ChooseShape2.shape2 = "zigzag"
             print(ChooseShape.shape1)
         }
