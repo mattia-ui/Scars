70d9cb50c6d63c02ed7458369a8bd02fe56a9108
Silvio <silviocresci1996@gmail.com>
2020-05-08 18:44:32 +0200

    a maronn

--- Scar/Cella2.swift
+++ Scar/Cella2.swift
@@ -12,20 +12,7 @@ import UIKit
 class Cella2: UITableViewCell{
     
     @IBOutlet weak var button: UIButton!
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        if(button.isSelected){
-            button.isSelected = false
-            PrimaTutorial().setSelected(stringa: "", n: button.tag )
-        }else{
-            button.isSelected = true
-            let s : String = String(String(describing: button.title(for: .normal)).dropFirst(10).dropLast(2))
-            print("prima \(PrimaTutorial().selected[button.tag])")
-            PrimaTutorial().selected[button.tag].append(s)
-            print("dopo \(PrimaTutorial().selected[button.tag])")
-
-            
-        }
-    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
