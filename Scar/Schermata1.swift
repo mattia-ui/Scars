d216686cb298cc21beb99fa253429fbf8483158e
scarsTeam <antonio.cimino97@libero.it>
2020-05-25 17:12:45 +0200

    fix

--- Scar/Schermata1.swift
+++ Scar/Schermata1.swift
@@ -183,7 +183,7 @@ class Schermata1: UIViewController, UICollectionViewDataSource, UICollectionView
         while(sqlite3_step(stmt) == SQLITE_ROW){
             w = Int(String(cString: sqlite3_column_text(stmt, 1))) ?? 0
         }
-        numeroWeek.text = "n.\(w)"
+        numeroWeek.text = "n.\(w + 1)"
         
         if(month2 > m || day2 > day){
             queryString = "UPDATE Date SET mese = '\(month2)', giorno = '\(day2)', ora = '\(hour2)', minuti = '\(minute2)';"
